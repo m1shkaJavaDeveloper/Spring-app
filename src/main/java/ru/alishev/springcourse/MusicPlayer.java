6380e285d87fb52c71da447bf7beb75c286945a6
Михаил <m1shka1998@mail.ru>
2020-12-14 16:26:39 +0300

    DZ6

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,38 +1,51 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList= new ArrayList<>();
 
-    private String name;
-    private int volume;
+//    private String name;
+//    private int volume;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getVolume() {
+//        return volume;
+//    }
+//
+//    public void setVolume(int volume) {
+//        this.volume = volume;
+//    }
 
-    public String getName() {
-        return name;
+    public List<Music> getMusicList() {
+        return musicList;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public int getVolume() {
-        return volume;
-    }
+    //    public MusicPlayer() {}
 
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
 
-    public MusicPlayer() {}
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 
     public void playMusic(){
-        System.out.println("Playing: "+music.getSong());
+        for (Music m: musicList) {
+            System.out.println(m.getSong());
+        }
     }
 }
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -13,9 +13,6 @@ public class TestSpring {
         MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-
         context.close();
     }
 }
