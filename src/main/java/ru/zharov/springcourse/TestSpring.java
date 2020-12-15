9deaf4eabf31fc6e68865a6a20ff1fd72a83c1da
Михаил <m1shka1998@mail.ru>
2020-12-15 15:48:41 +0300

    10th lesson

--- src/main/java/ru/zharov/springcourse/TestSpring.java
+++ src/main/java/ru/zharov/springcourse/TestSpring.java
@@ -8,32 +8,8 @@ public class TestSpring {
           "applicationContext.xml"
         );
 
-         Music music = context.getBean("rockMusic", Music.class);
-         MusicPlayer musicPlayer = new MusicPlayer(music);
-         musicPlayer.playMusic();
-
-         Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-        musicPlayer2.playMusic();
-//        Music music = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-//        MusicPlayer firstMusicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
-//
-//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-//
-//        System.out.println(comparison);
-//
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        firstMusicPlayer.setVolume(10);
-//
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
-
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
