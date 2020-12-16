1b4e47c458232145341ff089f8896277b0f9d588
Михаил <m1shka1998@mail.ru>
2020-12-16 11:54:56 +0300

    13th lesson

--- src/main/java/ru/zharov/springcourse/TestSpring.java
+++ src/main/java/ru/zharov/springcourse/TestSpring.java
@@ -1,11 +1,14 @@
 package ru.zharov.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.swing.*;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-          "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
 
 //        Computer computer = context.getBean("computer", Computer.class);
