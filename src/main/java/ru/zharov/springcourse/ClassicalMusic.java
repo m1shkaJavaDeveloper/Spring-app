55252e53acb78f8cdfb74d8ad7417950ba96df8a
Михаил <m1shka1998@mail.ru>
2020-12-16 10:57:58 +0300

    12th lesson

--- src/main/java/ru/zharov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/zharov/springcourse/ClassicalMusic.java
@@ -1,9 +1,23 @@
 package ru.zharov.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class ClassicalMusic implements Music{
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
 
     @Override
     public String getSong() {
